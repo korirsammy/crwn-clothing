ca1957be346de8c607290390b4b29251ad2f8322
Korir <sammy.korir@bell.corp.bce.ca>
2020-01-23 16:14:34 -0400

    Updated menu-items to naviagte with higher order component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import './menu-item.styles.scss'
+import { withRouter } from 'react-router-dom';
 
-const MenuItem=({title,imageUrl,size})=>(
-    <div  className={`${size} menu-item`}   >
+import './menu-item.styles.scss';
 
-    <div 
-    className='background-image'
-    style={{
-        backgroundImage:`url(${imageUrl})`
-    }} 
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
     />
-
     <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP NOW</span>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-    </div>
-
+  </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
 const HomePage=()=>(
     <div className='homepage'>
-       <Directory/>
-      
+       <Directory/>      
     </div>
 );
 
